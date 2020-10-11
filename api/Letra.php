b16a6aaed6939d303f414be22ddef7a7ba890a41
BeneditoBraimo <beneditobraimo@gmail.com>
2020-10-10 22:27:03 -0700

    -- adicionada a classe 'Letra.php'

---
+++ api/Letra.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Description of Letra
+ *
+ * @author Benedito Braimo
+ */
+class Letra {
+    private $caractere;
+    public function __construct($caractere) {
+        $this->caractere = $caractere;
+    }
+    
+    public function getCaractere() {
+        return $this->caractere;
+    }
+
+    public function setCaractere($caractere) {
+        $this->caractere = $caractere;
+        return $this;
+    }
+
+    public function mostrarCaractere() {
+        return $letra = $this->caractere;
+        }
+}
