4ff120d898ec5321285cce0af258b32168c536c0
BeneditoBraimo <beneditobraimo@gmail.com>
2020-10-11 23:48:27 -0700

    -- o ficheiro 'DBConnectionSetting.php' adicionado

---
+++ connectionSettings/DBConnectionSetting.php
@@ -0,0 +1,23 @@
+<?php
+
+$servidor = "localhost";
+$username = "root";
+$password = "";
+$database = "glossario_de_informatica";
+
+/*
+ * criar a conexao a base de dados MySQL
+ */
+
+$conexao = new mysqli_connect($servidor, $username, $password, $database);
+
+/*
+ * verificar a conexao a base de dados MySQL
+ */
+
+if($conexao ->mysqli_connect_error){
+    die("N&atilde;o &eacute; poss&iacute;vel conectar-se a base de dados".mysqli_connect_error);
+}
+
+echo '<p>Conectado</p>';
+mysqli_set_charset($database, 'utf-8');
